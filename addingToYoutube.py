a05489728539a7f3ae7fdba7b3e6a80889d29f29
shivansh mishra <shivanshmishra@shivanshs-MacBook-Pro.local>
2019-10-21 11:57:07 +0530

    script to add musict to yt-music playlist

--- addingToYoutube.py
+++ addingToYoutube.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import pdb
-
 import pickle
 
 with open("songnames.pkl", "rb") as f:
